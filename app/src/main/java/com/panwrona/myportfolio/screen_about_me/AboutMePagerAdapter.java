0bd9f4194eda7263aeaf57d7adb1f355d1ff4eaf
mariusz <mariusz.brona@droidsonroids.pl>
2015-10-13 16:31:09 +0200

    added some things

--- app/src/main/java/com/panwrona/myportfolio/screen_about_me/AboutMePagerAdapter.java
+++ app/src/main/java/com/panwrona/myportfolio/screen_about_me/AboutMePagerAdapter.java
@@ -10,12 +10,12 @@ import com.panwrona.myportfolio.screen_about_me.screen_experience.ExperienceFrag
 
 public class AboutMePagerAdapter extends FragmentPagerAdapter {
 	private static final int ABOUT_ME_POSITION = 0;
-	private static final int EXPERIENCE_POSITION = 1;
-	private static final int EDUCATION_POSITION = 2;
+	private static final int EXPERIENCE_POSITION = 2;
+	private static final int EDUCATION_POSITION = 1;
 	private static final int PAGE_COUNT = 3;
 
 	private final Context mContext;
-	private String tabTitles[] = new String[] { "About Me", "Experience", "Education" };
+	private String tabTitles[] = new String[] { "About Me", "Education", "Experience"};
 
 	public AboutMePagerAdapter(FragmentManager fm, Context context) {
 		super(fm);
